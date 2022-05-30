999537de82f0bc1889ccdeaddbed3e270dd47086
李铭辉 <1018596741@qq.com>
2022-05-30 23:40:37 +0800

    chore: add tailwindcss

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    postcss: {
+        plugins: [
+          require('tailwindcss')('./tailwind.config.js'),
+          require('autoprefixer')
+        ]
+      }
   }
