41846a1c469cda6d4a68c09da388befe2a825da3
李铭辉 <1018596741@qq.com>
2022-06-29 22:56:38 +0800

    feat: 完成首页景深特效

--- docs/components/Home/index.vue
+++ docs/components/Home/index.vue
@@ -1,19 +1,121 @@
 <template>
-    <div class="test"></div>
+<div class="bg-wrapper">
+        <div
+            class="bg-wrapper-item"
+            v-for="(item , idx) in artListRenderConfig"
+            :key="idx"
+            :style="item.style"
+        ></div>
+    </div>
+    <div class="test">
+        content
+    </div>
+    
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, computed, onMounted } from 'vue';
+
+const artEleList = [
+            {
+                bgSrc:`../../assets/home_bg_1.png`,
+                scrollSpeed:2,
+                yOffset:0,
+                yPosition:-50,
+            },
+            {
+                bgSrc:`../../assets/home_bg_2.png`,
+                scrollSpeed:5,
+                yOffset:0,
+                yPosition:-150,
+            },
+            {
+                bgSrc:`../../assets/home_bg_3.png`,
+                scrollSpeed:11,
+                yOffset:0,
+                yPosition:-200,
+            },
+            {
+                bgSrc:`../../assets/home_bg_4.png`,
+                scrollSpeed:16,
+                yOffset:0,
+                yPosition:-100,
+            },
+            {
+                bgSrc:`../../assets/home_bg_5.png`,
+                scrollSpeed:26,
+                yOffset:0,
+                yPosition:200,
+            },
+            {
+                bgSrc:`../../assets/home_bg_6.png`,
+                scrollSpeed:36,
+                yOffset:0,
+                yPosition:50,
+            },
+            {
+                bgSrc:`../../assets/home_bg_7.png`,
+                scrollSpeed:49,
+                yOffset:0,
+                yPosition:0,
+            },
+        ]
+
+
 
 export default defineComponent({
     name: 'Home',
     setup() {
-        
+        const artList = ref(artEleList);
+        const artListRenderConfig = computed(() => {
+            return artList.value.map((item, idx) => {
+                const transformStyle = `transform: translate3d(0px, ${item.yOffset}px, 0px)`;
+                return {
+                    style: `background-image:url(${item.bgSrc});
+                    ${transformStyle};${item.yPosition? 
+                    `background-position-y:${item.yPosition}px`: ''}
+                    `
+                }
+            })
+        });
+
+        onMounted(() => {
+            window.addEventListener("scroll", (event) => {
+                const top = window.pageYOffset;
+                const nums = artList.value.length;
+                for(let i=0;i < nums; i++) {
+                    const ele = artList.value[i];
+                    ele.yOffset = -(top * ele.scrollSpeed / 100)
+                }
+            })
+        })
+        return {
+            artListRenderConfig,
+        }
     },
 })
 </script>
 <style lang="less" scoped>
+.bg-size {
+    height: 750px;
+}
+.bg-wrapper {
+    .bg-size;
+    position: relative;
+    background: #b7cce0;
+    z-index: 10;
+    &-item {
+        position: fixed;
+        background-position: bottom center;
+        background-size: auto;
+        background-repeat: repeat-x;
+        width: 100%;
+        .bg-size
+    }
+}
 .test {
-    height: 2000px;
-    background-image: url('../../assets/home_bg_1.png');
+     position: relative;
+    height: 3000px;
+    background: #18252c;
+    z-index: 98
 }
 </style>
